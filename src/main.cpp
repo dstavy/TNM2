ea3551681ab5d34fb71c4f74029a6cc91a3b5439
dan stavy <dstavy@gmail.com>
2019-09-05 15:56:09 +0200

    main works! yay

--- src/main.cpp
+++ src/main.cpp
@@ -11,31 +11,45 @@ int main( ){
     // override : use internal signal handler (optional)
     //  verbose : print more log information (optional)
     //ofxWatchdog::boot(10000, true, true, true);
-    
+    /**/
     
     ofGLFWWindowSettings settings;
     settings.setGLVersion(3, 2); //we define the OpenGL version we want to use
 	
 	settings.setSize(2160, 1920);
+	//settings.setSize(1920, 2160);
+	//settings.setSize(1080, 1920);
+	//settings.setSize(1920, 1080 * 2);
 	
-#ifdef TARGET_WIN32
+#//ifdef TARGET_WIN32
 	settings.decorated = false;
-settings.setPosition(glm::vec2(1280, 0));
-#endif
+	// settings.windowMode = OF_FULLSCREEN;
+	//settings.multiMonitorFullScreen = true;
+	//settings.windowMode = OF_FULLSCREEN;
+	//settings.setPosition(glm::vec2(1280.0f, 0.0f));
+	// settings.setPosition(glm::vec2(0, 0));
+	//window.setMultiDisplayFullscreen(true);
+	settings.setSize(2160, 1920);
+	settings.setPosition(ofVec2f(1280, 0));
+
+	//settings.monitor = 1;
+	//settings.resizable = false;
+	//settings.resizable = true;
+//#endif
     
 #ifdef DEBUG
      settings.multiMonitorFullScreen = false;
 #else
-    settings.multiMonitorFullScreen = true;
-	settings.windowMode = OF_FULLSCREEN;
+   // settings.multiMonitorFullScreen = true;
+	//settings.windowMode = OF_WINDOW;
    //setMultiDisplayFullscreen (true);
 #endif
     
     ofCreateWindow(settings);
+  //  ofSetFullscreen(true);
 #ifdef DEBUG
     //ofSetFullscreen(false);
 #else
-  //  ofSetFullscreen(true);
 #endif
     // ofxWatchdog::trap(void)
     // ofxWatchdog::trap();
